f0bb0512f31d60d972436d84d114f0b127a66003
Jan Kelin <jan.kelin@applifting.cz>
2020-12-02 07:38:52 +0100

    Update dependencies

--- login.js
+++ login.js
@@ -1,21 +1,44 @@
 import express from 'express';
-import passport from 'passport';
-import BankId from 'passport-bankid';
+import qs from 'qs';
+import jwt from 'jsonwebtoken';
+import Axios from 'axios';
 
-const bankId = new BankId({
-  redirectUri: 'http://localhost:3000/',
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  sandbox: true,
-  scope: 'profile.name profile.addresses',
-});
-
-passport.use(bankId);
+const CLIENT_ID = '7261040c-a2b6-4688-8389-4dc9d77a1ba6';
+const CLIENT_SECRET =
+  'AL77jB6tZJMJOacM6ZUBWTk021G0YKC9uGleWYlWFlTk-GtfCh6s09xXkGLi8DFMGaZZ9MpfIHBGtyoxfwQ_dtA';
+const SCOPE = 'openid';
 
 const app = express();
 
-app.get('/', passport.authenticate('bankid'), async (req, res) =>
-  res.send(`Hello user with ID ${req.user.sub}!`)
-);
+app.get('/', async (req, res) => {
+  if (!req.query.code) {
+    res.redirect(
+      'https://core-idp.staging.ci.bankd.cz/auth?' +
+        qs.stringify({
+          redirect_uri: 'http://localhost:3000/',
+          response_type: 'code',
+          scope: SCOPE,
+          response_mode: 'query',
+          client_id: CLIENT_ID,
+        })
+    );
+  }
+
+  try {
+    const tokens = await Axios.post(
+      'https://core-idp.staging.ci.bankd.cz/token',
+      qs.stringify({
+        code: req.query.code,
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        grant_type: 'authorization_code',
+      })
+    );
+    const accessToken = jwt.decode(tokens.data.access_token);
+    res.send(`Hello user with ID ${accessToken.payload.sub}!`);
+  } catch (ex) {
+    console.error(ex.response.data);
+  }
+});
 
 app.listen(3000);
